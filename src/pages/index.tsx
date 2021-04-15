e02234d3266f3460472e3444b61da4188ebeaab3
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-15 20:56:18 -0300

    adding sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import style from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello world!</h1>
+    <h1 className={style.title}>Hello <span>world!</span></h1>
   );
 }
