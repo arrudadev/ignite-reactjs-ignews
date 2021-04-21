74261c5d103b1ae8475ec13f8ba08d088f025e64
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-21 11:55:59 -0300

    adding feature to validate if stripe customer already exists

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,57 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
 
+import { query } from 'faunadb';
+
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method === 'POST') {
     const session = await getSession({ req: request });
-    
-    const stripeConstomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    });
+
+    const user = await fauna.query<User>(
+      query.Get(
+        query.Match(
+          query.Index('user_by_email'),
+          query.Casefold(session.user.email)
+        )
+      )
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeConstomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
+
+      await fauna.query(
+        query.Update(
+          query.Ref(query.Collection('users'), user.ref.id),
+          { 
+            data: {
+              stripe_customer_id: stripeConstomer.id
+            }
+          }
+        )
+      );
+
+      customerId = stripeConstomer.id;
+    }
     
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeConstomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
